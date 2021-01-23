76b90cec41e3a43e376baadc34bbb9124b0efc4a
DongWendao <2521632021@qq.com>
2021-01-23 22:36:57 +0800

    登陆事件

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,18 +2,18 @@
 <template>
     <div>
         <!--el-form   表单-->
-        <el-form ref="form" :model="loginForm" class="loginContainer">
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
             <h3 class="loginTitle">系统登录</h3>
-            <el-form-item>
+            <el-form-item prop="username">
                 <!--输入框-->
                 <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item prop="password">
                 <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码"></el-input>
             </el-form-item>
 
-            <el-form-item>
+            <el-form-item prop="code">
                 <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码" class="loginCode"></el-input>
                 <img :src="captchaUrl">
             </el-form-item>
@@ -22,8 +22,6 @@
                 <el-checkbox v-model="checked" class="loginRemember">记住密码</el-checkbox>
                 <el-button type="primary" class="loginButton" @click="submitLogin">登陆</el-button>
             </el-form-item>
-
-
         </el-form>
     </div>
 </template>
@@ -35,11 +33,35 @@
             return{
                 captchaUrl:'',          /*验证码*/
                 loginForm:{
-                    username:'dwd',
+                    username:'',
                     password:'lsp',
                     code:''              /*验证码*/
                 },
-                checked:true
+                checked:true,
+
+                rules:{
+                    username:[{required:true,message:'用户名不能为空', trigger:'blur'}],
+                    password:[{required:true,message:'密码不能为空', trigger:'blur'}],
+                    code:[{required:true,message:'验证码不能为空', trigger:'blur'}]
+                }
+
+            }
+        }
+        ,
+        methods:{
+            submitLogin(){
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        this.$message({
+                            message: '恭喜你，提交成功',
+                            type: 'success'
+                        });
+                    } else {
+                        console.log('error submit!!');
+                        this.$message.error('错了哦，请输入完整信息');
+                        return false;
+                    }
+                });
             }
         }
     }
